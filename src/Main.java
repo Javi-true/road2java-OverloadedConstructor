ce3831a380f7fd0ca6c3a2cd38f6efbb66cdddad
Javier Trujillo <rivera.dev@outlook.com>
2024-12-15 04:01:20 -0600

    Overloaded constructor added. One class with diferent constructors with diferent parameters

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Pizza pizza = new Pizza("thick crust", "tomato", "mozzarella", "pepperoni");
+        System.out.println("Here are the ingredients of your pizza");
+        System.out.println(pizza.bread);
+        System.out.println(pizza.sauce);
+        System.out.println(pizza.cheese);
+        System.out.println(pizza.topping);
     }
 }
